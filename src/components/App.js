bed3b4ca6bfb8cf43085db365f3971b759df95e6
Usenmfon <usenmfonuko@gmail.com>
2021-07-09 20:28:40 +0100

    added contact project

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="contacts">
+            <div className="contact-card">
+                <h3>Mr. Wiskerson</h3>
+                <p>Phone: (212) 555-1234</p>
+                <p>Email: mr.wiskaz@catnap.com</p>
+            </div>
+
+            <div className="contact-card">
+                <h3>FluffyKins</h3>
+                <p>Phone: (212) 555-1234</p>
+                <p>Email: mr.wiskaz@catnap.com</p>
+            </div>
+
+            <div className="contact-card">
+                <h3>Destroyer</h3>
+                <p>Phone: (212) 555-1234</p>
+                <p>Email: mr.wiskaz@catnap.com</p>
+            </div>
+
+            <div className="contact-card">
+                <h3>Felix</h3>
+                <p>Phone: (212) 555-1234</p>
+                <p>Email: mr.wiskaz@catnap.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default App;
